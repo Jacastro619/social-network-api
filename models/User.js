d289d0aba57a1d67ad6f14511cc917fdecd96657
Jacastro619 <jorgecastro619@gmail.com>
2023-11-21 18:50:56 -0800

    Added completed models for db collections

--- models/User.js
+++ models/User.js
@@ -1,17 +1,45 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 const { isEmail } = require("validator");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trimmed: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [isEmail, { message: "Please enter a valid email" }],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: [isEmail, { message: "Please enter a valid email" }],
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
+
+const User = model("user", userSchema);
+
+module.exports = User;
